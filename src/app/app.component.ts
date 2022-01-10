f8bb6d861ed3eaed843eab98f6357600637d14cf
Liz-Le <nganle.512@gmail.com>
2022-01-10 12:40:41 -0400

    thirdcommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,33 +29,6 @@ ngOnInit(){
   }
   );
 
-  //Responsive Breakpoint
-  // this.breakpointObserver.observe(
-  //   [
-  //     Breakpoints.XSmall,
-  //     Breakpoints.Small,
-  //     Breakpoints.Medium,
-  //     Breakpoints.Large,
-  //     Breakpoints.XLarge
-  //   ]
-  // ).subscribe ( (state: BreakpointState) => {
-  //   if (state.breakpoints[Breakpoints.XSmall]) {
-  //     console.log ('Matches XSmall viewport');
-  //   }
-  //   if (state.breakpoints[Breakpoints.Small]){
-  //     console.log ('Matches Small viewport');
-  //   }
-  //   if (state.breakpoints[Breakpoints.Medium]) {
-  //     console.log ('Matches Medium viewport');
-  //   }
-  //   if (state.breakpoints[Breakpoints.Large]) {
-  //     console.log ('Matches Large viewport');
-  //   }
-  //   if (state.breakpoints[Breakpoints.XLarge]) {
-  //     console.log ('Matches XLarge viewport');
-  //   }
-  // });
-
 }
 }
 
